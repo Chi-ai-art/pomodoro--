011cb65fa507e91e686ff8e8eb28e03e3f1b2ff0
Chi-ai-art <nikonikosan0115@gmail.com>
2025-02-04 16:52:34 +0900

    Delete src/index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PomodoroTimer from './PomodoroTimer.jsx';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <PomodoroTimer />
-);
