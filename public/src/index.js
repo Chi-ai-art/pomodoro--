aed16036c94cdce9c83af957679d99919180a9d5
Chi-ai-art <nikonikosan0115@gmail.com>
2025-02-04 16:20:56 +0900

    Delete public directory

--- public/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PomodoroTimer from './PomodoroTimer';  // ←いま貼ってるファイル名と合わせる
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <PomodoroTimer />
-);
