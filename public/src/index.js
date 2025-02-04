daf3a6a25492bd4cafb0396fb7d051083b8bfdad
Chi-ai-art <nikonikosan0115@gmail.com>
2025-02-04 15:30:49 +0900

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PomodoroTimer from './PomodoroTimer';  // ←いま貼ってるファイル名と合わせる
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <PomodoroTimer />
+);
